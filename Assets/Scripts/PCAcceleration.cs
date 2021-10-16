cffd708e5513c452961d75f346d09ba5809fbbf0
Retoree <admin@1259.msk.ru>
2021-10-16 16:19:27 +0300

    PC Input Added

---
+++ Assets/Scripts/PCAcceleration.cs
@@ -0,0 +1,38 @@
+﻿using JoostenProductions;
+using Tools;
+using UnityEngine;
+
+public class PCAcceleration : BaseInputView
+{
+    public override void Init(SubscriptionProperty<float> leftMove, SubscriptionProperty<float> rightMove, float speed)
+    {
+        base.Init(leftMove, rightMove, speed);
+        UpdateManager.SubscribeToUpdate(Move);
+    }
+
+    private void OnDestroy()
+    {
+        UpdateManager.UnsubscribeFromUpdate(Move);
+    }
+
+    private void Move()
+    {
+        var direction = Vector3.zero;
+        direction.x = Input.GetAxis("Horizontal");
+
+        if (direction.x > 0)
+        {
+             if (direction.sqrMagnitude > 1)
+                        direction.Normalize();
+             OnRightMove(direction.sqrMagnitude / 200 * _speed);
+        }
+
+        else if (direction.x < 0)
+        {
+            if (direction.sqrMagnitude > 1)
+                direction.Normalize();
+            OnLeftMove(-direction.sqrMagnitude / 200 * _speed);
+        }
+            
+    }
+}
